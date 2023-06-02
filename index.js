b23e31e7df1ddc8b4af2514c250b9ae8c480ba2a
Jacob Meline <jacob.meline@drivetime.com>
2023-06-01 22:31:05 -0700

    Switch to use Axios for all network requests

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import * as cheerio from 'cheerio'
 import axios from 'axios'
 import fs from 'fs'
-import https from "https"
+import Path from 'path'
 
 const site = "https://hpaudiobooks.co"
 console.log(site)
@@ -25,36 +25,26 @@ const getAllPaginationLinks = async url => {
 }
 
 const downloadAudio = async (audioUrl, filename) => {
+  console.log(`Downloading audio as "${filename}"`);
   return new Promise(async (resolve, reject) => {
     const fileStream = fs.createWriteStream(filename);
-    https.get(audioUrl, (response) => {
-      response.pipe(fileStream);
 
-      fileStream.on('finish', () => {
-        console.log(`Audio downloaded successfully as "${filename}"`);
-        fileStream.close();
-        resolve();
-      });
+    const response = await axios({
+      method: "GET",
+      url: audioUrl,
+      responseType: 'stream'
+    })
 
-      fileStream.on('error', (error) => {
-        reject(error);
-      });
+    response.data.pipe(fileStream)
+    fileStream.on('finish', () => {
+      console.log(`Audio downloaded successfully as "${filename}"`);
+      fileStream.close();
+      resolve();
     });
 
-    // const response = await axios.get({
-    //   method: "get",
-    //   url: audioUrl,
-    // })
-    // response.data.pipe(fileStream)
-    // fileStream.on('finish', () => {
-    //   console.log(`Audio downloaded successfully as "${filename}"`);
-    //   fileStream.close();
-    //   resolve();
-    // });
-    //
-    // fileStream.on('error', (error) => {
-    //   reject(error);
-    // });
+    fileStream.on('error', (error) => {
+      reject(error);
+    });
   });
 };
 
@@ -64,12 +54,13 @@ const getAllAudioLinks = async (url, directory) => {
     console.log(`Created ${directory}`)
   }
   const audioLinks = await getAllPaginationLinks(url)
-  // console.log("audioLinks: ", audioLinks)
+  console.log("Grabbing all audioLinks: ", audioLinks)
+  const allResponses = await Promise.all(audioLinks.map(axios.get))
+  console.log("Finished grabbing all audioLinks: ", allResponses.length)
+
   const allAudioLinks = []
-  for (const link of audioLinks) {
-    const response = await axios.get(link)
+  for (const response of allResponses) {
     const $ = cheerio.load(response.data)
-    // const audioLink = $('audio source').attr('src')
     $('audio source').each((index, audio) => {
       const audioLink = $(audio).attr('src')
       const match = audioLink.match(/\/([^\/]+\.mp3)/)
@@ -77,7 +68,7 @@ const getAllAudioLinks = async (url, directory) => {
       if (match) {
         filename = match[1].replace(/%20/g, "_")
       }
-      const path = `${directory}/${filename}`
+      const path = Path.resolve(directory, filename)
       console.log(`AudioLink: '${audioLink}', path: '${path}'`)
       allAudioLinks.push(downloadAudio(audioLink, path))
     })
@@ -92,8 +83,6 @@ const stephenFryAudioBooks = await getBooks("https://hpaudiobooks.co/series/step
 console.log(jimDaleAudioBooks)
 // console.log(stephenFryAudioBooks)
 
-// const results = await getAudioLink(jimDaleAudioBooks[0])
-console.log("RESULTS!!!")
 await getAllAudioLinks(jimDaleAudioBooks[0], "hp_1_the_sorcerers_stone")
 await getAllAudioLinks(jimDaleAudioBooks[1], "hp_2_the_chamber_of_secrets")
 await getAllAudioLinks(jimDaleAudioBooks[2], "hp_3_the_prisoner_of_azkaban")
@@ -101,10 +90,3 @@ await getAllAudioLinks(jimDaleAudioBooks[3], "hp_4_the_goblet_of_fire")
 await getAllAudioLinks(jimDaleAudioBooks[4], "hp_5_the_order_of_the_phoenix")
 await getAllAudioLinks(jimDaleAudioBooks[5], "hp_6_the_half_blood_prince")
 await getAllAudioLinks(jimDaleAudioBooks[6], "hp_7_the_deathly_hollows")
-// console.log(await getAllAudioLinks(jimDaleAudioBooks[1]))
-// console.log(await getAllAudioLinks(jimDaleAudioBooks[2]))
-// console.log(await getAllAudioLinks(jimDaleAudioBooks[3]))
-// console.log(await getAllAudioLinks(jimDaleAudioBooks[4]))
-// console.log(await getAllAudioLinks(jimDaleAudioBooks[5]))
-// console.log(await getAllAudioLinks(jimDaleAudioBooks[6]))
-
